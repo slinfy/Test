167ca3a5b27549515cdf411c87f643bccdee8442
slinfy <mukesh_sharma@slinfy.com>
2014-08-08 14:58:10 +0530

    Test
    test

---
+++ test.php
@@ -0,0 +1,38 @@
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<meta property="og:title" content="Workday Sets Price Range for I.P.O." />
+<meta property="og:site_name" content="My Favorite News"/>
+<meta property="og:url" content="http://socialtyer.com" />
+<meta property="fb:app_id" content="[FB_APP_ID]" />
+<meta property="og:url" content="http://socialtyer.com" />
+<meta property="og:description" content="MyFavNews is the best source for your favorite news." />
+<meta property="og:image" content="http://socialtyer.com/images/place.jpg" />
+
+    
+</head>
+
+<body>
+<div
+  class="fb-like"
+  data-send="true"
+  data-width="450"
+  data-show-faces="true">
+</div>
+<a id="https://www.facebook.com/dialog/share?app_id=775905555754802&display=popup&href=http://www.weebuy.net&redirect_uri=http://www.weebuy.net">share</a>
+<a onclick='window.open("https://www.facebook.com/dialog/share?app_id=775905555754802&display=popup&href=http://www.weebuy.net&redirect_uri=http://www.weebuy.net", "MsgWindow", "width=200, height=100")'>facebook share</a>
+
+<a href="https://plus.google.com/share?url=http://www.weebuy.net/product/product_details/33" onclick="javascript:window.open(this.href,'http://www.weebuy.net/product/product_details/33', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
+  <img src="https://www.gstatic.com/images/icons/gplus-64.png" alt="Share on Google+"/></a>
+
+</body>
+</html>
+ 
+ 
+ 
+
+    
+    
+    
